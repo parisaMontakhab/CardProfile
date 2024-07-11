671aff683dee6de18dcb3decbc5bcba0ab23357c
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-07-11 11:50:59 +0200

    feat: add image to public folder

--- card/src/App.jsx
+++ card/src/App.jsx
@@ -5,15 +5,7 @@ function App() {
  
 
   return (
-    <div className='text-center'>
-     <div className='grid grid-rows-4 gap-2'>
-      <div>img</div>
-      <div>title</div>
-      <div>desc</div>
-      <div>skills</div>
-
-     </div>
-    </div>
+ 
   )
 }
 
