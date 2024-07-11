4b665daf15f4340cbe2c10eb4085b8596fd16c9d
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-07-11 12:14:47 +0200

    feat: add style

--- card/src/App.jsx
+++ card/src/App.jsx
@@ -2,11 +2,25 @@
 import './App.css'
 
 function App() {
- 
+ return(
+  <div className='text-center bg-red-100 m-40 w-1/3 mx-auto'>
+    <div className='grid grid-cols-2 gap-4'>
+      <div className=' bg-red-100'>
+        <img src='./public/images/profile.png' className='rounded-full  mx-auto p-4'/>
+      </div>
+      <div>
+        <div className='p-4'>
+          <h1 className='text-stone-600 font-bold text-2xl'>Parisa MontakhabiSani</h1>
+          <p className='text-stone-500 mt-4'><span className='font-bold'>Frontend-Developer</span>, I live in Rome, at this time I work by myself at home as  a freelancer but I'm looking for  a new position ;)</p>
+        </div>
 
-  return (
- 
-  )
+        <div>skills</div>
+      </div>
+
+    </div>
+
+  </div>
+ )
 }
 
 export default App
