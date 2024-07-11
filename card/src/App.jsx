4a46baffa40c9e3e850cbae0d89ebe8516786da7
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-07-11 11:20:20 +0200

    first commit

---
+++ card/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div className='text-center'>
+     <div className='grid grid-rows-4 gap-2'>
+      <div>img</div>
+      <div>title</div>
+      <div>desc</div>
+      <div>skills</div>
+
+     </div>
+    </div>
+  )
+}
+
+export default App
